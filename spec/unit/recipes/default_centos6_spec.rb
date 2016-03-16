30abad057fc53d6a0fcfc47ad67a6ee26d3ca478
Jeremy MAURO <j.mauro@criteo.com>
2016-03-16 12:39:29 +0100

    Setting platform_version for chefspec

--- spec/unit/recipes/default_centos6_spec.rb
+++ spec/unit/recipes/default_centos6_spec.rb
@@ -9,7 +9,10 @@ require 'spec_helper'
 describe 'kernel-modules::default' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::ServerRunner.new(
+        platform: 'centos',
+        version: '6.7',
+      )
       runner.converge(described_recipe)
     end
 
