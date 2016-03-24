3c51e6c999fed49c0fbd6bd1d67aab28da501e26
Jeremy MAURO <j.mauro@criteo.com>
2016-03-24 16:22:38 +0100

    Support RHEL 7 and change provider actions
    * Better provider splitting
    * Adding RHEL/CentOS 7 implementation for the provider
    * Adding every modprobe command as a resource
    
    test

--- resources/default.rb
+++ resources/default.rb
@@ -17,18 +17,37 @@
 # limitations under the License.
 #
 
+require 'chef/mixin/shell_out'
+include Chef::Mixin::ShellOut
+
 resource_name :kernel_module
-default_action :save
+allowed_actions :configure, :load, :unload, :remove
+
+default_action :load
 
 # Module name
-property :module, kind_of: String, name_property: true
+property :module_name, kind_of: String, name_property: true
+# Module will be load at boot time
+property :onboot, kind_of: [TrueClass, FalseClass], default: true
+# Allow the module reload in case of options changes
+property :reload, kind_of: [TrueClass, FalseClass], default: false
+# Allow to force reload the module. This options is only useful when 'reload' is
+# enable
+property :force_reload, kind_of: [TrueClass, FalseClass], default: false
 
-# "modprobe" command configuration (man morpdobe.conf)
-#   alias: [
-#     'usb-uhci uhci-hcd',
-#     'usb-ohci ohci-hcd',
-#   ]
-#   install: [
-#     'eth1394 /bin/true',
-#   ]
-property :modprobe, kind_of: Hash, default: nil
+# Type of modprobe resource_name (man modprobe.conf)
+property :alias, kind_of: [String, Array, NilClass], default: nil
+property :options, kind_of: [String, Array, NilClass], default: nil
+property :install, kind_of: [String, NilClass], default: nil
+property :remove, kind_of: [String, NilClass], default: nil
+property :blacklist, kind_of: [TrueClass, FalseClass, NilClass], default: nil
+
+def blacklisted?
+  # Ref:
+  # https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/6/html/Deployment_Guide/Blacklisting_a_Module.html
+  blacklist || install == '/bin/true' || install == '/bin/false'
+end
+
+def loaded?
+  @loaded ||= shell_out("/sbin/lsmod | grep -q -w ^#{module_name}").exitstatus == 0
+end
