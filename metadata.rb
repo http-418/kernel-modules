168266a51cc478ad97fb44e825bc85a9cf36ee50
Jeremy MAURO <j.mauro@criteo.com>
2016-07-12 17:41:47 +0200

    Bumping new version 0.1.2
    Change-Id: I6654d1849042b2fb1b8ad328bf09a25ad27eafc7

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description        'Manage kernel modules on linux'
 long_description   IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url         'https://github.com/criteo-cookbooks/kernel-modules.git'  if respond_to?(:issue_url)
 source_url         'https://github.com/criteo-cookbooks/kernel-modules.git'  if respond_to?(:source_url)
-version            '0.1.1'
+version            '0.1.2'
 chef_version       '>= 12.5.1' if respond_to?(:chef_version)
 supports           'rhel', '>= 6.0'
 supports           'centos', '>= 6.0'
