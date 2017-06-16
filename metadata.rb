6013c04ff4cc83dc55d42defe8b19c8c29791881
Jeremy MAURO <j.mauro@criteo.com>
2017-06-16 17:44:42 +0200

    Fix supermarket deploy + Bump 2.0.2
    Change-Id: If87fc78a19e60f02eaec2066e62f99d22cdcb278

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,9 @@ description      'Manage kernel modules on linux'
 long_description 'kernel-modules Cookbook
 Configure and Load kernel modules.
 '
-issues_url       'https://github.com/criteo-cookbooks/kernel-modules/issues'
-source_url       'https://github.com/criteo-cookbooks/kernel-modules'
-version          '2.0.1'
+issues_url       'https://github.com/criteo-cookbooks/kernel-modules/issues' if respond_to?(:issues_url)
+source_url       'https://github.com/criteo-cookbooks/kernel-modules' if respond_to?(:source_url)
+version          '2.0.2'
 supports         'redhat', '>= 6.0'
 supports         'centos', '>= 6.0'
 chef_version     '>= 12.5'
